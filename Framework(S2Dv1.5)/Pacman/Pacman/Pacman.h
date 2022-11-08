7a7181fc03fcbe4f90fa02a72ef1a96ddde771ba
Uhunoma Osemwengie <o012542m@student.staffs.ac.uk>
2022-11-08 14:57:33 +0000

    Finished timebased struct and methods

--- Framework(S2Dv1.5)/Pacman/Pacman/Pacman.h
+++ Framework(S2Dv1.5)/Pacman/Pacman/Pacman.h
@@ -14,43 +14,88 @@
 // Reduces the amount of typing by including all classes in S2D namespace
 using namespace S2D;
 
+
+struct Player
+{
+	float speedMultiplier;
+	int currentFrameTime;
+	int direction;
+	int frame;
+	Rect* sourceRect;
+	Texture2D* texture;
+	Vector2* position;
+	const int frameTime = 160;
+	const float speed = 0.1f;
+
+};
+
+struct Collect
+{
+	int frame;
+	int currentFrameTime;
+	int frameCount;
+	Rect* rect;
+	Texture2D* blueTexture;
+	Texture2D* invertedTexture;
+	const int frameTime = 600;
+};
+
+struct Menu
+{
+	Texture2D* background;
+	Rect* rectangle;
+	Vector2* stringPosition;
+	bool paused;
+	bool pKeyDown;
+	bool startGame;
+	Vector2* cordstringPosition;
+};
+
+
+
+
+
 // Declares the Pacman class which inherits from the Game class.
 // This allows us to overload the Game class methods to help us
 // load content, draw and update our game.
 class Pacman : public Game
 {
 private:
-	// Data to represent Pacman
-	Vector2* _pacmanPosition;
-	Rect* _pacmanSourceRect;
-	Texture2D* _pacmanTexture;
-	int _pacmanDirection;
-	int _pacmanFrame;
-	int _pacmanCurrentFrameTime;
-	const int _cpacmanFrameTime;
+	//// Data to represent Pacman
+	//Vector2* _pacmanPosition;
+	//Rect* _pacmanSourceRect;
+	//Texture2D* _pacmanTexture;
+	//int _pacmanDirection;
+	//int _pacmanFrame;
+	//int _pacmanCurrentFrameTime;
+	//const int _cpacmanFrameTime;
 
-	// Data to represent Munchie
-	int _frameCount;
-	Rect* _munchieRect;
-	Texture2D* _munchieBlueTexture;
-	Texture2D* _munchieInvertedTexture;
-	const int _cMunchieFrameTime;
-	int _munchieFrame;
-	int _munchieCurrentFrameTime;
+	//// Data to represent Munchie
+	//int _frameCount;
+	//Rect* _munchieRect;
+	//Texture2D* _munchieBlueTexture;
+	//Texture2D* _munchieInvertedTexture;
+	//const int _cMunchieFrameTime;
+	//int _munchieFrame;
+	//int _munchieCurrentFrameTime;
 
-	// Position for String
-	Vector2* _stringPosition;
+	//// Position for String
+	//Vector2* _stringPosition;
 
-	// Constant data for Game variables 
-	const float _cPacmanSpeed;
+	//// Constant data for Game variables 
+	//const float _cPacmanSpeed;
 
-	// Data for menu
-	Texture2D* _menuBackground;
-	Rect* _menuRectangle;
-	Vector2* _menuStringPosition;
-	bool _paused;
-	bool _pKeyDown;
-	bool _startGame;
+	//// Data for menu
+	//Texture2D* _menuBackground;
+	//Rect* _menuRectangle;
+	//Vector2* _menuStringPosition;
+	//bool _paused;
+	//bool _pKeyDown;
+	//bool _startGame;
+
+	Player* _pacman;
+	Collect* _munchie;
+	Menu* _pausenmain;
 
 	//Input methods 
 	void Input(int elapsedTime, Input::KeyboardState* state);
